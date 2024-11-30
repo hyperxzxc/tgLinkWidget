b5a1dbdf71530bb67fe19e4bd07631ab026630f6
hyper <hyperxzxc@gmail.com>
2024-11-30 04:53:09 +0300

    init

--- assets/js/tg.js
+++ assets/js/tg.js
@@ -45,9 +45,10 @@ class TelegramBtn {
         r.setAttribute("href", o), r.setAttribute("class", c), r.setAttribute("style", "text-decoration: none"), r.setAttribute("target", "_blank"), a.appendChild(r);
         let d = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         d.setAttribute("id", "hyperx_telegram_widget"), d.setAttribute("viewBox", "0 0 16 16"), d.setAttribute("width", s), d.setAttribute("height", s), r.appendChild(d);
-
+        let m = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        m.setAttribute("id", "telegram_icon_path"), m.setAttribute("cx", "50%"), m.setAttribute("cy", "50%"), m.setAttribute("r", "45%"), m.setAttribute("fill", "#FFFFFF"), d.appendChild(m);
         let _ = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        _.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"), _.setAttribute("id", "telegram_icon_path"), d.appendChild(_);
+        _.setAttribute("d", "M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"), _.setAttribute("id", "telegram_icon_path"), _.setAttribute("fill", "#40B3E0"), d.appendChild(_);
         let w = this.ga, g = this.fb, v = this.pi;
         if (1 == n) {
             let t = document.querySelector(".hyperx_telegram a.link_telegram_toggle"),
